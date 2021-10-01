34e43f057f03d48c8a3fefa1f2f82769811e4ab8
Saurav Panda <30600906+Sauravp98@users.noreply.github.com>
2021-10-01 23:34:57 +0530

    Update binary.java

--- binary.java
+++ binary.java
@@ -4,20 +4,22 @@ class binary
 	public static boolean binarysrc(int a[],int l,int h,int src)
 	{
 		while(h>=l){
-		int mid =l+((h-l)/2);
-		if(a[mid]==src){
-			return true;
+			//finding mid
+			int mid =l+((h-l)/2);
+			if(a[mid]==src){
+				return true;
+			}
+			else if(a[mid]>=src)
+				h=mid-1;
+			else
+				l=mid+1;
 		}
-		else if(a[mid]>=src)
-			h=mid-1;
-		else
-			l=mid+1;
-	}
 		return false;
 	}
 	public static void main(String args[])
 	{
 		Scanner sc = new Scanner(System.in);
+		//Accepting input
 		int n = sc.nextInt();
 		int a[] = new int[n];
 		for(int i=0;i<n;i++)
